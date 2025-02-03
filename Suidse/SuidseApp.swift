eb7ba57ce3ebeace052f2c9f5466013bc2c38b83
jnomded <james.edmond04@gmail.com>
2025-02-02 22:12:18 -0600

    Initial Commit

---
+++ Suidse/SuidseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuidseApp.swift
+//  Suidse
+//
+//  Created by James Edmond on 2/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SuidseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SuidseTests/SuidseTests.swift
@@ -0,0 +1,17 @@
+//
+//  SuidseTests.swift
+//  SuidseTests
+//
+//  Created by James Edmond on 2/2/25.
+//
+
+import Testing
+@testable import Suidse
+
+struct SuidseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
